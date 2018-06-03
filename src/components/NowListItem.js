36cb731062e62950b7aa2dfa96aa2d6b741ac255
jk2424 <jkim2244@gmail.com>
2018-06-03 13:12:25 -0400

    adding now playing scroll gallery

--- src/components/NowListItem.js
+++ src/components/NowListItem.js
@@ -9,65 +9,47 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-import {ORANGE, PINK, WHITE} from '../../styles';
+import {ORANGE, PINK, WHITE, GREYBG} from '../../styles';
 
-export default class UpcomingListItem extends Component {
+export default class NowListItem extends Component {
 
   render() {
     const {title} = this.props.data;
     return (
       <TouchableOpacity style={styles.container}>
+        <View style={styles.wrapper}>
           <Image style={styles.img}/>
-          <LinearGradient colors={[ORANGE, PINK]} style={styles.overlay}/>
           <Text style={styles.title}>{title}</Text>
+        </View>
         <View style={styles.imgshadow} />
       </TouchableOpacity>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
-    width: 320,
-    height: 180,
+    width: 140,
+    height: 280,
     marginRight: 20
   },
-  wrapper: {
-    borderRadius: 6,
-    zIndex: 1,
-    overflow: 'hidden',
-    width: 320,
-    height: 160
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: 320,
-    height: 160,
-    opacity: 0.3,
-    zIndex: 2
-  },
   title: {
-    fontSize: 20,
-    color: WHITE,
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    zIndex: 3
+    marginTop: 20,
+    fontSize: 15
   },
   img: {
     backgroundColor: 'rgb(230, 230, 230)',
-    width: 320,
-    height: 160
+    width: 140,
+    height: 210,
+    borderRadius: 6,
+    zIndex: 1
   },
   imgshadow: {
-    top: 16,
-    left: 16,
+    top: 20,
+    left: 12,
     backgroundColor: '#000',
     opacity: 0.5,
-    width: 293,
-    height: 144,
+    width: 123,
+    height: 185,
     zIndex: 0,
     position: 'absolute',
     shadowOffset: {
--- src/screens/MovieList.js
+++ src/screens/MovieList.js
@@ -3,16 +3,25 @@ import {
   StyleSheet,
   Text,
   FlatList,
-  View
+  View,
+  ScrollView
 } from 'react-native';
 
 import {ORANGE, PINK, WHITE, GREYBG} from '../../styles';
 
 import UpcomingListItem from '../components/UpcomingListItem';
+import NowListItem from '../components/NowListItem';
 
 export default class MovieList extends Component {
   state = {
     upcoming: [
+      {id:0, title: 'Test 1'},
+      {id:1, title: 'Test 2'},
+      {id:2, title: 'Test 3'},
+      {id:3, title: 'Test 4'},
+      {id:4, title: 'Test 5'}
+    ],
+    nowplaying: [
       {id:0, title: 'Test 1'},
       {id:1, title: 'Test 2'},
       {id:2, title: 'Test 3'},
@@ -27,20 +36,34 @@ export default class MovieList extends Component {
     <UpcomingListItem data={item} />
   );
 
+  renderNowPlaying = ({item}) => (
+    <NowListItem data={item} />
+  )
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>MOVIES</Text>
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>MOVIES</Text>
+      <FlatList
+        data={this.state.upcoming}
+        extraData={this.state}
+        keyExtractor={this.keyExtractor}
+        renderItem={this.renderUpcoming}
+        horizontal
+      />
+      <View style={styles.listcontainer}>
+        <Text>NOW</Text>
         <FlatList
-          data={this.state.upcoming}
+          data={this.state.nowplaying}
           extraData={this.state}
           keyExtractor={this.keyExtractor}
-          renderItem={this.renderUpcoming}
+          renderItem={this.renderNowPlaying}
           horizontal
         />
       </View>
-    );
-  }
+    </ScrollView>
+  );
+}
 }
 
 const styles = StyleSheet.create({
